438bb1459d5f388a2d66f14d66a5e1ac49aabed6
Syed Ali <m.ali@nexdegree.com>
2014-10-29 22:34:14 +0100

    Wrote file output code. Not checked, but should work.

--- FileOutput.cpp
+++ FileOutput.cpp
@@ -10,7 +10,7 @@
 
 FileOutput::FileOutput(string path)
 {
-    
+    this->path = path;
 }
 
 void FileOutput::write_params_to_file(double** means, double* variances, double** cov_matrix, int num_dims, int num_classes)
@@ -21,5 +21,30 @@ void FileOutput::write_params_to_file(double** means, double* variances, double*
     
     //Write params to a file conforming to usps.README
     
+	ofstream fout(this->path);
+	
+	fout << "d" << endl;
+	fout << num_classes << "\t: # of classes" << endl;
+	fout << num_dims << "\t: # dimension of observation vectors" << endl;
+	
+	for(int c=1; c<10; c++){ //classes
+		fout << c << "\t: class k=" << c << endl;
+		fout << p[c-1] << "\t: prior p(k=" << c << ")" << endl;
+		
+		//print mean vector for class c
+		for(int d=0; d<256; d++)
+			fout << means[c][d] << " ";
+			
+		fout << "\t: mean vector mu_{k=" << c << "} = (0,0)" << endl;
+		
+		//print co-variance vector for class c
+		for(int d=0; d<256; d++)
+			fout << cov_matrix[c][d] << " ";
+			
+		fout << "\t: vector of variances sigma^2_{k=" << c << "} = (1 1)" << endl;
+ 	}
+	
+	fout.close();
+	
     printf("Writing to file completed successfully!\n");
 }
